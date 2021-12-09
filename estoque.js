f34ec7ee6af24f5657cbc924cfd00bf30bd5b84c
Rafael Mazzutti <rafaelmazz22@gmail.com>
2021-12-09 13:15:29 -0300

    JS 0.2

---
+++ estoque.js
@@ -0,0 +1,112 @@
+let produtos = [
+    {
+        "nome":"",
+        "preco":1,
+        "estado": [""],
+        "specs": [""]
+    },
+    {
+        "nome":"Quantum MUV Up",
+        "preco":300,
+        "estado": ["Problema na Tela"],
+        "specs": ["Android 7.0 (64-Bit)", "MediaTek MT6753", "3 GB de RAM", 
+            "32 GB de ROM", "720 x 1280", "Fevereiro de 2017"]
+    },
+    {
+        "nome":"Nokia Lumia 520",
+        "preco":400,
+        "estado": ["Perfeito"],
+        "specs": ["Windows Phone 10 (32-bit)", "Snapdragon S4 Qualcomm", "512 MB de RAM", 
+            "8 GB de ROM", "480 x 800", "Fevereiro de 2013"]
+    },
+    {
+        "nome":"Samsung Galaxy Gran Prime Duos TV",
+        "preco":400,
+        "estado": ["Parte traseira com trinca."],
+        "specs": ["Android 5.1 (32-bit)", "Qualcomm Snapdragon 410", "1 GB de RAM", "8 GB de ROM", 
+            "540 x 960", "Março de 2014"]
+    },
+    {
+        "nome":"LG G2 Lite",
+        "preco":200,
+        "estado": ["Modulo WIFI/BT Queimado."],
+        "specs": ["Android 5.1 (32-bit)", "Qualcomm Snapdragon 200", "1 GB de RAM", "4 GB de ROM", 
+            "480 x 800", "Março de 2014"]
+    },
+    {
+        "nome":"Nuu Mobile A3",
+        "preco":200,
+        "estado": ["Riscos na traseira."],
+        "specs": ["Android 6.0 (32-bit)", "Mediatek MT6580", "1 GB de RAM", "8 GB de ROM", 
+            "480 x 854", "Novembro de 2016"]
+    },
+    {
+        "nome":"LG K10 Novo (2017)",
+        "preco":400,
+        "estado": ["Bateria com defeito.", "Frenteira trincada."],
+        "specs": ["Android 8.1 (Atualizado do 7.0) (32-bit)", "MediaTek MT6750", "2 GB de RAM", "32 GB de ROM",
+            "720 x 1280", "Janeiro de 2017"]
+    },
+    {
+        "nome":"LG K11 Plus",
+        "preco":600,
+        "estado": ["Carcaça danificada.", "Traseira amaçada."],
+        "specs": ["Android 7.1.2 (CPU 64-bit, OS 32-bit)", "MediaTek MT6750", "3 GB de RAM", "32 GB de ROM", 
+            "720 x 1280", "Março de 2018"]
+    },
+    {
+        "nome":"LG G2",
+        "preco":300,
+        "estado": ["LCD danificado.", "Bateria danificada."],
+        "specs": ["Android 4.4.2 (32-bit)", "Snapdragon 800", "2 GB de RAM", "16 GB de ROM", 
+            "1080 x 1920", "Abril de 2017"]
+    },
+    {
+        "nome":"LG K4 Novo (2016)",
+        "preco":400,
+        "estado": ["Traseira danificada.", "Slot SD danificado."],
+        "specs": ["Android 6.0.1", "MediaTek MT6737M", "1 GB de RAM", "8 GB de ROM",
+            "480 x 854", "Abril de 2016"]
+    },
+    {
+        "nome":"LG Optimus L3 Dual",
+        "preco":200,
+        "estado": ["Perfeito."],
+        "specs": ["Android 2.3 (32-bit)", "ARM Cortex-A5", "512mb de RAM", "2 GB de ROM",
+            "240 x 320", "Janeiro de 2012"]
+    },
+    {
+        "nome":"Semp Toshiba IS1414",
+        "preco":800,
+        "estado": ["Usado."],
+        "specs": ["OpenSUSE Leap 15.3 (64-bit)", "Intel Pentium T4500 Dual-Core", "2 GB de RAM", "320 GB de HDD",
+            "1366 x 768", "2012"]
+    },
+    {
+        "nome":"Dell Inspiron n4050",
+        "preco":600,
+        "estado": ["Usado"],
+        "specs": ["Microsoft Windows 10 (64-bit)", "Intel i5 Mobile 2° Geração", "4 GB de RAM", "1 TB de HDD", 
+            "1366 x 768", "2012"]
+    },
+    {
+        "nome":"Acer Aspire v3-571",
+        "preco":800,
+        "estado": ["Teclado danificado."],
+        "specs": ["Fedora Workstation 34 GNOME (64-bit)", "Intel i7 Mobile 3° Geração", "6 GB de RAM", "512 GB de HDD", 
+            "1366 x 768", "2013"]
+    },
+    {
+        "nome":"Tablet ICC Vision I37W (Braview)",
+        "preco":400,
+        "estado": ["LCD com 1 linha."],
+        "specs": ["Microsoft Windows 8.1 Enterprise (32-bit)", "Intel Atom Mobile 3° Geração", "1 GB de RAM",
+            "16 GB de SD", "720 x 1280",  "2014"]
+    },
+    {
+        "nome":"KZ ES4",
+        "preco":100,
+        "estado": ["Usado"],
+        "specs": ["W 24g", "Plug 3.5mm", "Pin 0.75",  "20-40000hz", "25 imp", "104db/mW", "2018"]
+    },
+];
---
+++ index.js
@@ -0,0 +1,87 @@
+let voltar = document.getElementById("voltar");
+let nomeprod = document.getElementById("nomeprod");
+let proximo = document.getElementById("proximo");
+
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+
+let barrinha = document.getElementById("barrinha");
+let barrinhaA = document.getElementById("barrinhaA");
+
+let uau = document.getElementById("uau");
+
+//sim o destaque do dia eh aleatorio, n conte pra ngm
+let posi = Math.floor(Math.random() * (produtos.length - 1)) + 1; //ignorar o produto teste
+
+let progresso = 0;
+let progressoA = 5;
+
+//carregar os produtos
+function recarregar(up)
+{
+    //resetar o timer
+    progresso = 0;
+    progressoA = 5;
+
+    switch(up)
+    {
+        default:
+        case 0: break;
+        case 1: //voltar
+        {
+            posi -= 1;
+            if (posi < 1)
+            {
+                posi = produtos.length - 1;
+            }
+            break;
+        }
+        case 2: //proximo
+        {
+            posi += 1;
+            if (posi > produtos.length - 1)
+            {
+                posi = 1;
+            }
+        }
+    }
+
+    img1.src = `estoque/imagens/${posi}a.jpg`;
+    img2.src = `estoque/imagens/${posi}b.jpg`;
+    nomeprod.text = produtos[posi].nome;
+    nomeprod.href = "";
+    uau.textContent = `Com ${produtos[posi].specs[0]}, apenas R$${produtos[posi].preco},00!`;
+}
+
+voltar.onclick = function()
+{
+    recarregar(1);
+}
+
+proximo.onclick = function()
+{
+    recarregar(2);
+}
+
+window.setInterval(function() 
+{
+    if (img1.matches(':hover') || 
+        img2.matches(':hover') || 
+        nomeprod.matches(':hover') || 
+        voltar.matches(':hover') ||
+        proximo.matches(':hover'))
+        return;
+
+    progresso += 1;
+    progressoA -= 1;
+    if (progresso > 5)
+    {
+        recarregar(2);
+    }
+
+    barrinha.textContent = ">>>>>>>>".repeat(progresso);
+    barrinhaA.textContent = ">>>>>>>>".repeat(progressoA);
+},  1000);
+
+
+recarregar(0); //shhhh
